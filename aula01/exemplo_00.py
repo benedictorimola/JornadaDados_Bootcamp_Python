2fa0109bbe5e0eee796e8125281ed5a5db9907d4
Benedicto Rimola <rimolabenedicto@gmail.com>
2024-08-20 23:15:06 -0300

    adicionando exemplo_00.py

---
+++ aula01/exemplo_00.py
@@ -0,0 +1,10 @@
+## CÓDIGOS FÁCEIS
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+print(len(input("Digite seu nome: "))) 
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma #
+print( int(input("Valor_1: ")) + int(input("Valor_2: ")) )
+valor_1 =  int(input("Informe o valor_1: "))
+valor_2 =  int(input("Informe o valor_2: "))
+val_tot = valor_1 + valor_2 
+print(val_tot) 
