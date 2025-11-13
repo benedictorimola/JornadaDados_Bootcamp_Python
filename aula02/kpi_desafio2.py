445ffd29737d933217ec3c74a221139ce644560f
Benedicto Rimola <rimolabenedicto@gmail.com>
2025-11-13 06:31:25 -0300

    Melhoria de tratamento da variável nome em  kpi_desafio2.py

--- aula02/kpi_desafio2.py
+++ aula02/kpi_desafio2.py
@@ -8,10 +8,13 @@ try:
     # Verifica se há números no nome
     elif any(char.isdigit() for char in nome):
         raise ValueError("O nome não deve conter números.")
+    elif nome.isspace():
+        raise ValueError("O nome deve ser preenchido")
     else:
         print("Nome válido:", nome)
 except ValueError as e:
     print(e)
+    exit()
 
 # Solicita ao usuário que digite o valor do seu salário e converte para float
 
@@ -21,6 +24,7 @@ try:
         print("Por favor, digite um valor positivo para o salário.")
 except ValueError:
     print("Entrada inválida para o salário. Por favor, digite um número.")
+    exit()
 
 # Solicita ao usuário que digite o valor do bônus recebido e converte para float
 try:
@@ -29,6 +33,7 @@ try:
         print("Por favor, digite um valor positivo para o bônus.")
 except ValueError:
     print("Entrada inválida para o bônus. Por favor, digite um número.")
+    exit()
 
 # Assumindo uma lógica de cálculo para o bônus final e KPI
 bonus_final = bonus_recebido * 1.2  # Exemplo, ajuste conforme necessário
